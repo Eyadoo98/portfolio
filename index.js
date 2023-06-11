926c8a8f186e3190caab49ba8afa90aaf37fd5ac
Eyad Jafar <eyad98.jafar@gmail.com>
2023-06-11 17:12:25 +0300

    [add] services , works , contact , footer section

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+const hLinks = document.querySelectorAll("#hLink");
 
+// for toggling menu show and hide
 hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+});
+
+// for closing menu when link is clicked
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     menu.classList.toggle("hidden");
     hamburger.classList.toggle("bg-white");
+  });
+});
+
+// for toggle dark mode
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
